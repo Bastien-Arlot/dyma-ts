e21d7c27267acc996a0017f5eb2a8ef7b87b0ab4
Bastien Arlot <arlot.b@gmail.com>
2022-07-11 17:32:32 +0200

    enum

--- src/test.js
+++ src/test.js
@@ -2,7 +2,35 @@ var foo = "blue";
 var bar = 2;
 var bool = false;
 var obj = {
-    name: 'jean'
+    name: "jean"
 };
 var anything = [1, 2, 3];
 var trym = "paris";
+var arr = [1, 2, 3, 4];
+var arr2 = [1, 2, 3, 4];
+var tuple = [1, 2];
+var tuple2 = [1, "deux"];
+var XhrReadyState;
+(function (XhrReadyState) {
+    XhrReadyState[XhrReadyState["UNSENT"] = 0] = "UNSENT";
+    XhrReadyState[XhrReadyState["OPENED"] = 1] = "OPENED";
+    XhrReadyState[XhrReadyState["HEADERS_RECEIVED"] = 2] = "HEADERS_RECEIVED";
+    XhrReadyState[XhrReadyState["LOADING"] = 3] = "LOADING";
+    XhrReadyState[XhrReadyState["DONE"] = 4] = "DONE";
+})(XhrReadyState || (XhrReadyState = {}));
+var readyState = 0;
+if (readyState === XhrReadyState.UNSENT) {
+    console.log("requete non envoyé");
+}
+else if (readyState === XhrReadyState.OPENED) {
+    console.log("requete ouverte");
+}
+else if (readyState === XhrReadyState.HEADERS_RECEIVED) {
+    console.log("en tete reçu");
+}
+else if (readyState === XhrReadyState.LOADING) {
+    console.log("Requete en chargement");
+}
+else if (readyState === XhrReadyState.DONE) {
+    console.log("Requete effectuée");
+}
--- src/test.ts
+++ src/test.ts
@@ -3,13 +3,35 @@ let bar: number = 2;
 let bool: boolean = false;
 
 const obj: object = {
-  name: 'jean',
+  name: "jean",
 };
 
-const anything: any = [1,2,3];
+const anything: any = [1, 2, 3];
 
 const trym: "paris" = "paris";
-const arr: number[] = [1,2,3,4];
-const arr2: Array<number> = [1,2,3,4];
-const tuple: [number, number] = [1,2]
+const arr: number[] = [1, 2, 3, 4];
+const arr2: Array<number> = [1, 2, 3, 4];
+const tuple: [number, number] = [1, 2];
 const tuple2: [number, string] = [1, "deux"];
+
+enum XhrReadyState {
+  UNSENT,
+  OPENED,
+  HEADERS_RECEIVED,
+  LOADING,
+  DONE,
+}
+
+const readyState = 0;
+
+if (readyState === XhrReadyState.UNSENT) {
+  console.log("requete non envoyé");
+} else if (readyState === XhrReadyState.OPENED) {
+  console.log("requete ouverte");
+} else if (readyState === XhrReadyState.HEADERS_RECEIVED) {
+  console.log("en tete reçu");
+} else if (readyState === XhrReadyState.LOADING) {
+  console.log("Requete en chargement");
+} else if (readyState === XhrReadyState.DONE) {
+  console.log("Requete effectuée");
+}
