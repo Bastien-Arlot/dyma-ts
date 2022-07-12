a8e7ef1696bd65750dc2955a75970380b7e29041
Bastien Arlot <arlot.b@gmail.com>
2022-07-12 15:29:36 +0200

    functions

--- src/index.ts
+++ src/index.ts
@@ -22,16 +22,36 @@ enum XhrReadyState {
   DONE = 33,
 }
 
-const readyState = 33;
+// const readyState = 33;
 
-if (readyState === XhrReadyState.UNSENT) {
-  console.log("requete non envoyé");
-} else if (readyState === XhrReadyState.OPENED) {
-  console.log("requete ouverte");
-} else if (readyState === XhrReadyState.HEADERS_RECEIVED) {
-  console.log("en tete reçu");
-} else if (readyState === XhrReadyState.LOADING) {
-  console.log("Requete en chargement");
-} else if (readyState === XhrReadyState.DONE) {
-  console.log("Requete effectuée");
+// if (readyState === XhrReadyState.UNSENT) {
+//   console.log("requete non envoyé");
+// } else if (readyState === XhrReadyState.OPENED) {
+//   console.log("requete ouverte");
+// } else if (readyState === XhrReadyState.HEADERS_RECEIVED) {
+//   console.log("en tete reçu");
+// } else if (readyState === XhrReadyState.LOADING) {
+//   console.log("Requete en chargement");
+// } else if (readyState === XhrReadyState.DONE) {
+//   console.log("Requete effectuée");
+// }
+
+// console.log(XhrReadyState.HEADERS_RECEIVED);
+
+const foo2: null = null;
+const foo3: undefined = undefined;
+
+function add(nbr1: number, nbr2:number):number {
+  return nbr1 + nbr2;
 }
+
+console.log(add(2,4));
+
+let add2: (nbr1: number, nbr2: number) => number;
+
+add2 = (nbr1, nbr2) => {
+  return nbr1 + nbr2
+}
+
+console.log(add2(3,2));
+
